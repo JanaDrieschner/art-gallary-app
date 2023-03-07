c629b7a5ee8971c005682de703e7fd0bfcc15311
Jana Drieschner-Aronova <j.drieschner@hotmail.com>
2023-03-07 16:19:53 +0100

    wip:list of all pieces

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
+import useSWR from "swr";
+import { useEffect, useState } from "react";
+import ArtPieces from "../components/ArtPieces/index.js";
+
 export default function HomePage() {
-  return (
-    <div>
-      <h1>Hello from Next.js</h1>
-    </div>
-  );
+  // const { data } = useSWR("https://example-apis.vercel.app/api/art");
+  const [data, setData] = useState([]);
+  // console.log("data", data);
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch("https://example-apis.vercel.app/api/art");
+      if (response.ok) {
+        const newData = await response.json();
+        console.log("data", newData);
+        setData(newData);
+      } else {
+        console.log(response.statusText);
+      }
+    }
+    getData();
+  }, []);
+  return <ArtPieces pieces={data} />;
 }
